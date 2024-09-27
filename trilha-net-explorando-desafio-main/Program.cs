081578ecd702b1aaf594c50f5c66d01683881691
Hilton Santana Bastos <hiltonsantanabastos@gmail.com>
2024-09-27 11:23:26 -0300

    Desafio de projeto
    Concluído!

---
+++ trilha-net-explorando-desafio-main/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using DesafioProjetoHospedagem.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p3 = new Pessoa(nome: "Hóspede 3");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+hospedes.Add(p3);
+
+// Cria a suíte com capacidade 2 (suficiente para os 2 hóspedes)
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 3, valorDiaria: 30);
+
+// Cria uma nova reserva, passando a suíte e os hóspedes
+Reserva reserva = new Reserva(diasReservados: 20);
+reserva.CadastrarSuite(suite);
+
+try
+{
+    reserva.CadastrarHospedes(hospedes);
+    // Exibe a quantidade de hóspedes e o valor da diária
+    Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+    Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro: {ex.Message}");
+}
